2a8fae5f6463e3d8d54359b351b92e7dd9984a8b
Alex <digwnews@gmail.com>
2016-04-17 13:36:02 +0300

    fitness works

--- genetic.py
+++ genetic.py
@@ -2,7 +2,7 @@ import random
 from chromosome import Chromosome
 
 dim = 5
-chromosome_length = dim - 2
+chromosome_length = dim  # min is (dim - 2)
 
 weights = [
     [0,    5,   2,   100,  7],
@@ -12,9 +12,22 @@ weights = [
     [7,    4,  13,    12,  0]
 ]
 
+quite = False
+
 
 class GeneNetwork(object):
     def __init__(self, dim, weights, chromosome_length, source, destination):
+        """
+
+        :param dim: problem dimension
+        :param weights:
+        :param chromosome_length: length of chromosome
+        :param source: source node
+        :param destination: destination node
+        :return:
+        """
+        if source >= dim - 1 or destination >= dim - 1:
+            raise ValueError
         self.chromosome_length = chromosome_length
         self.dim = dim
         self.weights = weights
@@ -24,6 +37,12 @@ class GeneNetwork(object):
         self.population_size = 0
 
     def start(self, gen_max, pop_size):
+        """
+
+        :param gen_max: maximum number of generations
+        :param pop_size: initial population size
+        :return:
+        """
         gen = 1  # from first generation
         self.generate_population(pop_size)  # generate initial population
         self.population_size = pop_size
@@ -33,8 +52,10 @@ class GeneNetwork(object):
             while p <= self.population_size:
                 p += 1
                 parents = random.sample(range(self.population_size), 2)
-                newbie = self.crossover(*parents)
+                newbie = self.crossover(self.population[parents[0]], self.population[parents[1]])
                 newbie.mutate()
+                fit = self.fitness(newbie)
+                pass
 
 
     def generate_population(self, n):
@@ -60,15 +81,23 @@ class GeneNetwork(object):
         return Chromosome(chromosome)
 
     def crossover(self, mother, father):
+        """
+
+        :param mother: first parent
+        :param father: second parent
+        :return: crossing over child
+        """
         mother_list = mother.get()
         father_list = father.get()
         cut = random.randint(0, self.chromosome_length - 1)
-        child = mother_list[0:cut] + father_list[cut + 1:]
+        child = mother_list[0:cut] + father_list[cut:]
         return Chromosome(child)
 
-
+    def fitness(self, chromosome):
+        chromosome_list = chromosome.get()
+        return sum([self.weights[i][j] for i, j in zip(chromosome_list[:-1], chromosome_list[1:])])
 
 
 if __name__ == "__main__":
-    gene_network = GeneNetwork(dim, weights, chromosome_length, 3, 7)
-    gene_network.start(50, 10)
+    gene_network = GeneNetwork(dim, weights, chromosome_length, 3, 1)
+    gene_network.start(50, 10)  # start with 50 generations and 10 initial chromosomes
